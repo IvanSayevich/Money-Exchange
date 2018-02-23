817fff520c29b833f467fbab982c52bf6142ae95
IvanSayevich <sundial@tut.by>
2018-02-23 10:06:33 +0300

    money exchnge

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    const   array = [50,25,10,5,1];
+            names = ["H","Q","D","N","P"];
+    let result = {};
+
+    function getChange(money1){
+        let money2 = money1;
+        array.forEach(function(element,index){
+            if (money2 >= element){
+                result[names[index]] = (Math.floor(money2/element));
+                money2 = money2%element;
+            }
+        });
+    }
+    if(currency <= 0){
+        return {};
+    }else if(currency > 10000){
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"}
+    }else{
+        getChange(currency);
+        return result;
+    }
 }
